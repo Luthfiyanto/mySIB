9c149730b94096e3036b5f9e324ff3e45ac1433b
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2023-08-29 12:33:07 +0700

    complete JS-array challenge

--- Chapter2/JS-array-challenge/scripts/sortCarByYearAscendingly.js
+++ Chapter2/JS-array-challenge/scripts/sortCarByYearAscendingly.js
@@ -1,13 +1,27 @@
 function sortCarByYearAscendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
+  // console.log(cars);
 
   // Clone array untuk menghindari side-effect
   // Apa itu side effect?
-  const result = [...cars];
-
+  let result = cars;
   // Tulis code-mu disini
+  let len = cars.length;
+  for (let i = len - 1; i >= 0; i--) {
+    for (let j = 1; j <= i; j++) {
+      // Corrected inner loop condition
+      if (cars[j - 1].year > cars[j].year) {
+        let temp = cars[j - 1];
+        cars[j - 1] = cars[j];
+        cars[j] = temp;
+      }
+    }
+  }
 
   // Rubah code ini dengan array hasil sorting secara ascending
+
   return result;
+  // return cars;
 }
+
+console.log(sortCarByYearAscendingly(cars));
--- Chapter2/JS-array-challenge/scripts/sortCarByYearDecendingly.js
+++ Chapter2/JS-array-challenge/scripts/sortCarByYearDecendingly.js
@@ -1,13 +1,27 @@
 function sortCarByYearDescendingly(cars) {
   // Sangat dianjurkan untuk console.log semua hal hehe
-  console.log(cars);
+  // console.log(cars);
 
   // Clone array untuk menghindari side-effect
   // Apa itu side effect?
-  const result = [...cars];
+  // const result = [...cars];
+  let result = cars;
 
   // Tulis code-mu disini
+  let len = cars.length;
+  for (let i = len - 1; i >= 0; i--) {
+    for (let j = 1; j <= i; j++) {
+      // Corrected inner loop condition
+      if (cars[j - 1].year < cars[j].year) {
+        let temp = cars[j - 1];
+        cars[j - 1] = cars[j];
+        cars[j] = temp;
+      }
+    }
+  }
 
   // Rubah code ini dengan array hasil sorting secara descending
   return result;
 }
+
+console.log(sortCarByYearDescendingly(cars));
