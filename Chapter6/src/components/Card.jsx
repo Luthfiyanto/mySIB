4d1fa130b75ed33dcccc448ce814e6374c17ac13
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2023-11-06 03:13:06 +0700

    add file images

--- Chapter6/src/components/Card.jsx
+++ Chapter6/src/components/Card.jsx
@@ -1,19 +1,19 @@
 import cap from "../assets/img/fi_users.svg";
 import setting from "../assets/img/fi_settings.svg";
 import calendar from "../assets/img/fi_calendar.svg";
-import image from "../assets/img/carDefault.jpg";
 
 function Card({ car }) {
-  const { manufacture, model, rentPerDay, description, capacity, transmission, year } = car;
+  const { image, manufacture, model, rentPerDay, description, capacity, transmission, year } = car;
 
   const reversedValue = String(rentPerDay).split("").reverse().join("");
   const groups = reversedValue.match(/.{1,3}/g);
   const formattedValue = groups.join(".").split("").reverse().join("");
 
+  const imageSrc = `../../public/${image}`;
   return (
     <div className="card shadow-sm border-0">
       <div className="card-body card_car">
-        <img src={image} alt={manufacture} />
+        <img src={imageSrc} alt={manufacture} />
         <p className="car-title">{`${manufacture} ${model}`}</p>
         <h5>Rp {formattedValue} / hari</h5>
         <p className="desc">{description}</p>
